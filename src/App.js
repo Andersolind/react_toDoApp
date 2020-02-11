074156fa578991bff98f2285e2bf27cb734dc285
Svenska <svenska@Svenskas-MacBook-Pro.local>
2020-02-11 16:52:11 -0500

    added finalized app

--- src/App.js
+++ src/App.js
@@ -1,90 +1,24 @@
 import React, { useState } from "react";
-import "./App.css";
-
-const Todo = ({ todo, completeTodo, removeTodo }) => {
-  const remove = e => {
-    e.preventDefault();
-    removeTodo(todo);
-  };
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "nowrap",
-        justifyContent: "flex-start",
-        alignContent: "stretch",
-        alignItems: "flex-start"
-      }}
-    >
-      <div
-        onClick={() => completeTodo(todo)}
-        className="todo"
-        style={{
-          flex: "1 1 auto",
-          cursor: "pointer",
-          textDecoration: todo.isCompleted ? "line-through" : ""
-        }}
-      >
-        {todo.text}
-      </div>
-      <button
-        type="button"
-        style={{ width: "3em", cursor: "pointer" }}
-        onClick={e => remove(e)}
-      >
-        x
-      </button>
-    </div>
-  );
-};
-
-const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "nowrap",
-        justifyContent: "flex-start",
-        alignContent: "stretch",
-        alignItems: "flex-start"
-      }}
-    >
-      <input
-        style={{ width: "75%" }}
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <button type="submit" style={{ flex: "1 1 auto", cursor: "pointer" }}>
-        Add
-      </button>
-    </form>
-  );
-};
+import cx from "classnames";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [newTodoValue, setNewTodoValue] = useState("");
 
-  const completedTodos = () => {
-    const completed = todos.filter(item => item.isCompleted === true);
+  const submitForm = e => {
+    e.preventDefault();
+    if (!newTodoValue) return;
+    addTodo(newTodoValue);
+    setNewTodoValue("");
+  };
+
+  const remainingTodos = () => {
+    const completed = todos.filter(item => item.isCompleted !== true);
     return completed.length;
   };
 
   const addTodo = text => {
-    const newTodos = [...todos, { text }];
+    const newTodos = [...todos, { text, isCompleted: false }];
     setTodos(newTodos);
   };
 
@@ -94,25 +28,50 @@ function App() {
     setTodos(todos);
   };
 
-  const removeTodo = todo => {
-    const index = todos.indexOf(todo);
-    todos.splice(index, 1);
-    setTodos(todos);
-  };
-
   return (
     <div className="app">
       <div className="todo-list">
-        <TodoForm addTodo={addTodo} />
-        Completed {completedTodos()}/{todos.length}
-        {todos.map((todo, index) => (
-          <Todo
-            key={`${index}_${todo.value}`}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
+        <form
+          onSubmit={submitForm}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "nowrap",
+            justifyContent: "flex-start",
+            alignContent: "stretch",
+            alignItems: "flex-start"
+          }}
+        >
+          <input
+            style={{ width: "75%" }}
+            type="text"
+            className="input"
+            value={newTodoValue}
+            onChange={e => setNewTodoValue(e.target.value)}
           />
-        ))}
+          <button type="submit" style={{ flex: "1 1 auto", cursor: "pointer" }}>
+            Add
+          </button>
+        </form>
+        <div className="task-counter">
+          {remainingTodos()} remaining out of {todos.length} tasks
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li
+              key={`${index}_${todo.value}`}
+              onClick={() => completeTodo(todo)}
+              className={cx({ "is-done": todo.isComplete })}
+              style={{
+                cursor: "pointer",
+                zIndex: 0,
+                textDecoration: todo.isCompleted ? "line-through" : ""
+              }}
+            >
+              {todo.text}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
