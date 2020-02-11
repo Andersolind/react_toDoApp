0fed0379534ea102bd0ab101952dd17913a1959c
Svenska <svenska@Svenskas-MacBook-Pro.local>
2020-02-10 19:49:47 -0500

    added todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+const Todo = ({ todo, completeTodo, removeTodo }) => {
+  const remove = e => {
+    e.preventDefault();
+    removeTodo(todo);
+  };
   return (
     <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "flex-start",
+        alignContent: "stretch",
+        alignItems: "flex-start"
+      }}
     >
-      {todo.text}
-
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
+      <div
+        onClick={() => completeTodo(todo)}
+        className="todo"
+        style={{
+          flex: "1 1 auto",
+          cursor: "pointer",
+          textDecoration: todo.isCompleted ? "line-through" : ""
+        }}
+      >
+        {todo.text}
       </div>
+      <button
+        type="button"
+        style={{ width: "3em", cursor: "pointer" }}
+        onClick={e => remove(e)}
+      >
+        x
+      </button>
     </div>
   );
-}
+};
 
-function TodoForm({ addTodo }) {
+const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
   const handleSubmit = e => {
@@ -28,63 +50,69 @@ function TodoForm({ addTodo }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        justifyContent: "flex-start",
+        alignContent: "stretch",
+        alignItems: "flex-start"
+      }}
+    >
       <input
+        style={{ width: "75%" }}
         type="text"
         className="input"
         value={value}
         onChange={e => setValue(e.target.value)}
       />
+      <button type="submit" style={{ flex: "1 1 auto", cursor: "pointer" }}>
+        Add
+      </button>
     </form>
   );
-}
+};
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: "Learn about React",
-      isCompleted: false
-    },
-    {
-      text: "Meet friend for lunch",
-      isCompleted: false
-    },
-    {
-      text: "Build really cool todo app",
-      isCompleted: false
-    }
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  const completedTodos = () => {
+    const completed = todos.filter(item => item.isCompleted === true);
+    return completed.length;
+  };
 
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
 
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
-    setTodos(newTodos);
+  const completeTodo = todo => {
+    const index = todos.indexOf(todo);
+    todos[index].isCompleted = !todos[index].isCompleted;
+    setTodos(todos);
   };
 
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+  const removeTodo = todo => {
+    const index = todos.indexOf(todo);
+    todos.splice(index, 1);
+    setTodos(todos);
   };
 
   return (
     <div className="app">
       <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        Completed {completedTodos()}/{todos.length}
         {todos.map((todo, index) => (
           <Todo
-            key={index}
-            index={index}
+            key={`${index}_${todo.value}`}
             todo={todo}
             completeTodo={completeTodo}
             removeTodo={removeTodo}
           />
         ))}
-        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
